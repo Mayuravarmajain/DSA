50aa671b5869c0e70e9d434775f45811905148e8
Mayura Varma Jain <131279586+Mayuravarmajain@users.noreply.github.com>
2026-09-01 22:27:37 +0530

    Time: 3 ms (82.70%) | Memory: 16.2 MB (14.93%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -0,0 +1,18 @@
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.prefix = [0] * (len(nums) + 1)
+
+        for i in range(len(nums)):
+            self.prefix[i + 1] = self.prefix[i] + nums[i]
+
+    def sumRange(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        return self.prefix[right + 1] - self.prefix[left]
